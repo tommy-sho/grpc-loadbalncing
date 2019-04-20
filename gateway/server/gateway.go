6c55ee6ba228e45c533e19a67a021b04f5dea89b
tomiokashougo <tomiokasyogo@gmail.com>
2019-04-21 00:54:15 +0900

    change directory structure

---
+++ gateway/server/gateway.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"context"
+	"fmt"
+
+	proto "github.com/tommy-sho/grpc-loadbalncing/app/gateway/genproto"
+)
+
+type GatewayService struct {
+	backendClient proto.BackendServerClient
+}
+
+func NewGatewaySerive(bClient proto.BackendServerClient) *GatewayService {
+	return &GatewayService{bClient}
+}
+
+func (g *GatewayService) Greeting(ctx context.Context, req *proto.GreetingRequest) (*proto.GreetingResponse, error) {
+	in := &proto.MessageRequest{
+		Name: req.Name,
+	}
+	r, err := g.backendClient.Message(ctx, in)
+	if err != nil {
+		return &proto.GreetingResponse{}, fmt.Errorf("gateway backendClinet error : %v ", err)
+	}
+
+	res := &proto.GreetingResponse{
+		Message: r.Message,
+	}
+	return res, nil
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/tommy-sho/grpc-loadbalncing
+
+go 1.12
+
+require (
+	github.com/golang/protobuf v1.3.1
+	github.com/urfave/cli v1.20.0 // indirect
+	google.golang.org/grpc v1.20.1
+)
